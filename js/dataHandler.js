aa4c24080c46f45c1588bee2736db9c8fc14005b
ZOE-ZION\Yanceyworks <ej.waban@gmail.com>
2022-09-12 03:36:30 +0800

    Initialize dataHandler methods in class dataHandler

--- js/dataHandler.js
+++ js/dataHandler.js
@@ -1,5 +1,31 @@
-class Handler{
-    constructor(){
-        
+class dataHandler{
+    constructor(){        
+        const cart = sessionStorage.getItem('cart')
+        if(cart){
+            console.log('Session Exists')
+        }
+        else{
+            console.log('Session Does Not Exists')
+            sessionStorage.setItem("cart",'')
+            var jsonData
+        }
+    }
+
+    // Add data in DataHandler
+    pushData(data){
+        let jsonData = JSON.stringify(data)
+        sessionStorage.setItem("cart",jsonData)
+    }
+
+    fetchData(){
+        let see = JSON.parse(sessionStorage.getItem('cart'))
+        return see
+    }
+
+    // Update data in DataHandler
+    updateData(data){
+        var jsonItems = JSON.stringify(data)
+        sessionStorage.setItem("cart", jsonItems)  
+        shoppingCart.addCart(data);      
     }
 }
